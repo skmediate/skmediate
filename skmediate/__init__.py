3476451a791cd77a72ad06a0416ed28c5c7f2d0c
Manjari Narayan (SCG) <mnarayan@users.noreply.github.com>
2020-12-01 16:14:48 -0800

    Created package skeleton

---
+++ skmediate/__init__.py
@@ -0,0 +1,13 @@
+# type: ignore[attr-defined]
+"""A scikit-learn compatible package for performing mediational regression and causal mediation analysis"""
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
