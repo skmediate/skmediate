c5ef8e5ea65ca125111c4aa1a1858ff77c6cc9dc
Manjari Narayan (SCG) <mnarayan@users.noreply.github.com>
2020-12-23 11:25:10 -0800

    Added test_datasets.py

---
+++ skmediate/tests/test_datasets.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import pytest
+
+from skmediate.datasets import make_null_mediation, make_mediation
+
+from sklearn.utils._testing import assert_array_almost_equal
+from sklearn.utils._testing import assert_raises
+
+
+def test_make_null_mediation():
+    pass
+
+
+def test_make_mediation():
+    pass
