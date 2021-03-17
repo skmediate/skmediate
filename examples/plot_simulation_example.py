3ae40d12b9cba9a2865fdecc74679218256f6c34
Manjari Narayan (CC-skmediate) <mnarayan@users.noreply.github.com>
2021-03-17 17:43:57 +0000

    reformat

--- examples/plot_simulation_example.py
+++ examples/plot_simulation_example.py
@@ -1,25 +1,32 @@
 """
-Simulation Example
-===============================
+Simulation Example.
 
-Shows the different combinations of parameters used to simulate null and true data.
+===============================
+Shows the different combinations of parameters used to
+simulate null and true data.
 
 """
 
 import numpy as np
 from datasets import make_null_mediation, make_mediation
 
-outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(dag_type='null-dag1')
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(
+    dag_type="null-dag1"
+)
 print(true_alpha)
 print(np.round(np.transpose(true_beta), 1))
 print(np.round(np.transpose(true_gam)), 1)
 
-outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(dag_type='null-dag2')
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(
+    dag_type="null-dag2"
+)
 print(true_alpha)
 print(np.round(np.transpose(true_beta), 1))
 print(np.round(np.transpose(true_gam)), 1)
 
-outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(dag_type='null-dag3')
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(
+    dag_type="null-dag3"
+)
 print(true_alpha)
 print(np.round(np.transpose(true_beta), 1))
 print(np.round(np.transpose(true_gam)), 1)
@@ -29,7 +36,9 @@ print(true_alpha)
 print(np.round(np.transpose(true_beta), 1))
 print(np.round(np.transpose(true_gam)), 1)
 
-outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_mediation(n_informative_mo=20)
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_mediation(
+    n_informative_mo=20
+)
 print(true_alpha)
 print(np.round(np.transpose(true_beta), 1))
 print(np.round(np.transpose(true_gam)), 1)
