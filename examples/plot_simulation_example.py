812b45af8d6a21aa691d3467bbca623c549755ae
Manjari Narayan (CC-skmediate) <mnarayan@users.noreply.github.com>
2021-03-17 22:46:58 +0000

    Example is more readable.

--- examples/plot_simulation_example.py
+++ examples/plot_simulation_example.py
@@ -11,34 +11,56 @@ import numpy as np
 from skmediate.datasets import make_null_mediation, make_mediation
 
 outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(
-    dag_type="null-dag1"
+    n_mediators=10, dag_type="null-dag1"
 )
-print(true_alpha)
-print(np.round(np.transpose(true_beta), 1))
-print(np.round(np.transpose(true_gam)), 1)
+print("Parameter values when simulating null confounding motif:")
+print("Alpha: {}".format(true_alpha))
+print("Beta: {}".format(np.round(np.transpose(true_beta), 1)))
+print("Gamma: {}".format(np.round(np.transpose(true_gam), 1)))
+total_ie = np.multiply(np.transpose(true_beta), true_gam)
+fraction_ie = np.round(np.divide(total_ie, total_ie + true_alpha), 2)
+print("Indirect-to-Total Signal Strength: {}".format(fraction_ie))
 
 outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(
-    dag_type="null-dag2"
+    n_mediators=10, dag_type="null-dag2"
 )
-print(true_alpha)
-print(np.round(np.transpose(true_beta), 1))
-print(np.round(np.transpose(true_gam)), 1)
+print("Parameter values when simulating null independent variables motif:")
+print("Alpha: {}".format(true_alpha))
+print("Beta: {}".format(np.round(np.transpose(true_beta), 1)))
+print("Gamma: {}".format(np.round(np.transpose(true_gam), 1)))
+total_ie = np.multiply(np.transpose(true_beta), true_gam)
+fraction_ie = np.round(np.divide(total_ie, total_ie + true_alpha), 2)
+print("Indirect-to-Total Signal Strength: {}".format(fraction_ie))
 
 outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(
-    dag_type="null-dag3"
+    n_mediators=10, dag_type="null-dag3"
 )
-print(true_alpha)
-print(np.round(np.transpose(true_beta), 1))
-print(np.round(np.transpose(true_gam)), 1)
-
-outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_mediation()
-print(true_alpha)
-print(np.round(np.transpose(true_beta), 1))
-print(np.round(np.transpose(true_gam)), 1)
+print("Parameter values when simulating null noise motif:")
+print("Alpha: {}".format(true_alpha))
+print("Beta: {}".format(np.round(np.transpose(true_beta), 1)))
+print("Gamma: {}".format(np.round(np.transpose(true_gam), 1)))
+total_ie = np.multiply(np.transpose(true_beta), true_gam)
+fraction_ie = np.round(np.divide(total_ie, total_ie + true_alpha), 2)
+print("Indirect-to-Total Signal Strength: {}".format(fraction_ie))
 
 outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_mediation(
-    n_informative_mo=20
+    n_mediators=10
 )
-print(true_alpha)
-print(np.round(np.transpose(true_beta), 1))
-print(np.round(np.transpose(true_gam)), 1)
+print("Parameter values when simulating true mediation motif, no sparsity:")
+print("Alpha: {}".format(true_alpha))
+print("Beta: {}".format(np.round(np.transpose(true_beta), 1)))
+print("Gamma: {}".format(np.round(np.transpose(true_gam), 1)))
+total_ie = np.multiply(np.transpose(true_beta), true_gam)
+fraction_ie = np.round(np.divide(total_ie, total_ie + true_alpha), 2)
+print("Indirect-to-Total Signal Strength: {}".format(fraction_ie))
+
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_mediation(
+    n_mediators=10, n_informative_mo=5
+)
+print("Parameter values when simulating true but partial mediation motif, 60% sparse :")
+print("Alpha: {}".format(true_alpha))
+print("Beta: {}".format(np.round(np.transpose(true_beta), 1)))
+print("Gamma: {}".format(np.round(np.transpose(true_gam), 1)))
+total_ie = np.multiply(np.transpose(true_beta), true_gam)
+fraction_ie = np.round(np.divide(total_ie, total_ie + true_alpha), 2)
+print("Indirect-to-Total Signal Strength: {}".format(fraction_ie))
