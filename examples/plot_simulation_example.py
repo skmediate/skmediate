c5c57095f0505e4fbe1835a64bad3d2bbae7355b
Manjari Narayan (CC-skmediate) <mnarayan@users.noreply.github.com>
2021-03-17 17:33:37 +0000

    Rewrite example using simulation functions

---
+++ examples/plot_simulation_example.py
@@ -0,0 +1,32 @@
+"""
+Simulation Example
+===============================
+
+Shows the different combinations of parameters used to simulate null and true data.
+
+"""
+
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(dag_type='null-dag1')
+print(true_alpha)
+print(np.round(np.transpose(true_beta),1))
+print(np.round(np.transpose(true_gam)),1)
+
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(dag_type='null-dag2')
+print(true_alpha)
+print(np.round(np.transpose(true_beta),1))
+print(np.round(np.transpose(true_gam)),1)
+
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_null_mediation(dag_type='null-dag3')
+print(true_alpha)
+print(np.round(np.transpose(true_beta),1))
+print(np.round(np.transpose(true_gam)),1)
+
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_mediation()
+print(true_alpha)
+print(np.round(np.transpose(true_beta),1))
+print(np.round(np.transpose(true_gam)),1)
+
+outcomes, exposures, mediators, true_alpha, true_beta, true_gam = make_mediation(n_informative_mo=20)
+print(true_alpha)
+print(np.round(np.transpose(true_beta),1))
+print(np.round(np.transpose(true_gam)),1)
